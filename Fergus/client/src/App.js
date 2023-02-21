a43005695c46ed8285d2c56346549957cd615a4a
Fergus2299 <114816708+Fergus2299@users.noreply.github.com>
2023-02-21 13:25:18 +0000

    changes

--- Fergus/client/src/App.js
+++ Fergus/client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter, Route, Routes, Link, NavLink} from 'react-router-dom';
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
+
 
 
 
@@ -14,10 +16,12 @@ function App() {
         <ul>
           <li><a href='/'>Home</a></li>
           <li><a href="/createpost">Create Post</a></li>
+          <li><a href="/post/:id"></a></li>
         </ul>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/createpost" element={<CreatePost />}/>
+          <Route path="/post/:id" element={<Post />}/>
         </Routes>
       </BrowserRouter>
     </div>
